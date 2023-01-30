23e0d07b159e15cc43b94dc21dbc2958037df2dd
yasledesma <yaslesd@gmail.com>
2023-01-30 18:20:34 -0300

    feat(pointers_and_error): add Withdraw function + tests

--- pointers_and_errors/wallet.go
+++ pointers_and_errors/wallet.go
@@ -1,13 +1,33 @@
 package wallet
 
+import (
+	"errors"
+	"fmt"
+)
+
+type Shitcoin int
+
 type Wallet struct {
-    balance int
+    balance Shitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (s Shitcoin) String() string {
+    return fmt.Sprintf("%d STC", s)
+}
+
+func (w *Wallet) Deposit(amount Shitcoin) {
     w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Shitcoin {
     return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Shitcoin) error {
+    if amount > w.balance {
+        return errors.New("Insufficient funds to perform this transaction.")
+    }
+
+    w.balance -= amount
+    return nil
 }
--- pointers_and_errors/wallet_test.go
+++ pointers_and_errors/wallet_test.go
@@ -3,15 +3,40 @@ package wallet
 import "testing"
 
 func TestWallet(t *testing.T) {
-    wallet := Wallet{}
-
-    wallet.Deposit(10)
-
-    got := wallet.Balance()
-    want := 10
-
-    if got != want {
-        t.Errorf("got %d, want %d.", got, want)
+    assertEquals := func(t testing.TB, wallet Wallet, want Shitcoin) {
+        t.Helper()
+        got := wallet.Balance()
+        if got != want {
+            t.Errorf("got %s, want %s.", got, want)
+        }
     }
+
+    t.Run("it should make a deposit into the wallet", func(t *testing.T) {
+        wallet := Wallet{}
+        wallet.Deposit(Shitcoin(10))
+        want := Shitcoin(10)
+
+        assertEquals(t, wallet, want)
+    }) 
+
+    t.Run("it should make a withdrawal from the wallet", func(t *testing.T) {
+        wallet := Wallet{10}
+        wallet.Withdraw(Shitcoin(5))
+        want := Shitcoin(5)
+
+        assertEquals(t, wallet, want)
+    })
+
+    t.Run("it should try to withdraw an amount greater than the current balance and fail", func(t *testing.T) {
+        startingBalance := Shitcoin(20)
+        wallet := Wallet{startingBalance} 
+        err := wallet.Withdraw(Shitcoin(50))
+
+        assertEquals(t, wallet, startingBalance)
+
+        if err == nil {
+            t.Error("wanted an error but didn't get one")
+        }
+    })
 }
 
