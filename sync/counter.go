609f18b42cd1a86cdf053ed4121e93a92e4f5aae
yasledesma <yaslesd@gmail.com>
2023-04-01 13:22:13 -0300

    feat(sync): add counter + tests

---
+++ sync/counter.go
@@ -0,0 +1,23 @@
+package counter
+
+import "sync"
+
+type Counter struct {
+	value int
+	mu sync.Mutex
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+func (c *Counter) Value() int {
+	return c.value
+}
+
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.value++
+}
+
